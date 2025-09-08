775d0052b275fe7a02885e6a77d531fb35db8ba0
shulamit <shul91a@gmail.com>
2025-09-08 20:24:30 +0300

    create DAL

---
+++ server/WorkHoursAPI/WorkHoursAPI/DAL/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using WorkHoursAPI.Models;
+
+namespace WorkHoursAPI.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(int id);
+        Task AddAsync(Employee employee);
+        Task UpdateAsync(Employee employee);
+        Task DeleteAsync(int id);
+    }
+}
+
---
+++ server/WorkHoursAPI/WorkHoursAPI/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkHoursAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; } // קוד עובד
+
+        [Required]
+        public string Tz { get; set; } // ת.ז
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; } // מאוחסן בהצפנה
+
+        public string Phone { get; set; }
+
+        public bool IsAdmin { get; set; } // הרשאת ניהול
+    }
+}
+}
